94738fc2b1c08ad2e35f39c3586c4b60366cb1bd
macmini <happyf-weallareeuropean@users.noreply.github.com>
2026-07-10 18:30:03 +0800

    fix: import OSLog in iOS logging call sites

--- iOS/VoiceInk-ios/LocalModelManager.swift
+++ iOS/VoiceInk-ios/LocalModelManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import OSLog
 import VoiceInkCore
 
 @MainActor
--- iOS/VoiceInk-ios/WhisperTranscriptionService.swift
+++ iOS/VoiceInk-ios/WhisperTranscriptionService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import OSLog
 import VoiceInkCore
 
 struct WhisperTranscriptionService: VoiceInkAudioTranscriptionService {
